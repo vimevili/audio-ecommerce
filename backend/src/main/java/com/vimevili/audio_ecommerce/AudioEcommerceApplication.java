58f624bfb8b8b15c85da9b3c0084bdd034c417e5
vimevili <vinicius_meirelless@hotmail.com>
2026-01-20 15:57:00 -0300

    initial commit

---
+++ backend/src/main/java/com/vimevili/audio_ecommerce/AudioEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.vimevili.audio_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioEcommerceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vimevili/audio_ecommerce/AudioEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimevili.audio_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
