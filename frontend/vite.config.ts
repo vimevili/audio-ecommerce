46e66c346f19f9cd0dc62960b020450dd82e84d1
vimevili <vinicius_meirelless@hotmail.com>
2026-01-26 15:04:22 -0300

    Add Tailwind CSS and TanStack Router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss(), tanstackRouter()],
+  resolve: {
+    alias: {
+      // Isso mapeia o '@' para a pasta 'src'
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
