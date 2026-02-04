44a4c6c0a9416d608142c5c912dc8ffeedd04806
vimevili <vinicius_meirelless@hotmail.com>
2026-02-04 17:42:35 -0300

    fix(api): update baseURL to use environment variables for production

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,7 +2,10 @@ import type { IStandardErrorInfo } from '@/interfaces/auth';
 import axios, { AxiosError } from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL:
+    import.meta.env.MODE === 'production'
+      ? 'https://audio-ecommerce.onrender.com/api'
+      : 'http://localhost:8080/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
